bd0c5216e8283cc1171b99b8df6715d450d50cc0
niknug <niknug@yandex.ru>
2024-11-17 02:47:52 +0300

    Added settings class

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,15 +2,19 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
     """Класс для управления ресурсами и поведением игры"""
     def __init__(self):
         """Инициализирует игру и создает игровые ресурсы"""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width,
+             self.settings.screen_height))
         pygame.display.set_caption('Alien Invasion')
-        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Запуск основного цикла игры"""
@@ -19,7 +23,7 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
 
             pygame.display.flip()
 
