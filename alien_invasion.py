f28535cda09e8118d24cffb030c05922f1bf7767
niknug <niknug@yandex.ru>
2024-11-17 02:33:59 +0300

    Created game window

---
+++ alien_invasion.py
@@ -0,0 +1,28 @@
+import sys
+
+import pygame
+
+class AlienInvasion:
+    """Класс для управления ресурсами и поведением игры"""
+    def __init__(self):
+        """Инициализирует игру и создает игровые ресурсы"""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption('Alien Invasion')
+        self.bg_color = (230, 230, 230)
+
+    def run_game(self):
+        """Запуск основного цикла игры"""
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            self.screen.fill(self.bg_color)
+
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
