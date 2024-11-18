13664b456cb502c5678cc9a8d59f6cac8de92bac
niknug <niknug@yandex.ru>
2024-11-19 02:14:34 +0300

    Showed ship on screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Класс для управления ресурсами и поведением игры"""
@@ -16,6 +17,8 @@ class AlienInvasion:
              self.settings.screen_height))
         pygame.display.set_caption('Alien Invasion')
 
+        self.ship = Ship(self)
+
     def run_game(self):
         """Запуск основного цикла игры"""
         while True:
@@ -24,6 +27,7 @@ class AlienInvasion:
                     sys.exit()
 
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             pygame.display.flip()
 
