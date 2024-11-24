9ca0b47cd939424897c9dc20e3688c634b498547
niknug <niknug@yandex.ru>
2024-11-24 23:58:24 +0300

    Added alien fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -91,8 +91,27 @@ class AlienInvasion:
             self.bullets.add(new_bullet)
 
     def _create_fleet(self):
-        """Создание флома пришельцев"""
+        """Создание флота пришельцев"""
         alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height -
+                             (3 * alien_height) - ship_height)
+        number_rows = available_space_y // (2 * alien_height)
+
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number, row_number):
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = alien_width + alien_number * alien_width * 2
+        alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + row_number * alien.rect.height * 2
         self.aliens.add(alien)
 
 
