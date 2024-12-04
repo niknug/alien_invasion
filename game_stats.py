50ec41a5dc8393ad101c097c6400c974db4c811f
niknug <niknug@yandex.ru>
2024-12-04 23:07:42 +0300

    Added high score

--- game_stats.py
+++ game_stats.py
@@ -7,6 +7,8 @@ class GameStats:
 
         self.game_active = False
 
+        self.high_score = 0
+
     def reset_stats(self):
         """Инициализирует статистику, изменяющуюся в ходе игры"""
         self.ships_left = self.settings.ship_limit
